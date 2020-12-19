2ed0152f8b792bf492c614df6ca222d763218622
owl0109 <kijimakenta19960109@gmail.com>
2020-12-19 18:00:11 +0900

    JSXの練習

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,18 @@
+import React,{Component} from 'react'
 
-function App() {
-  return (<div><h1>Hello World</h1></div>);
+class App extends Component{
+  render(){
+    //return<div>～の箇所がJSX。可読性が高い
+    //return<div>Hello World</div>
+    return<h1>Hello World</h1>
+
+    //内部処理はこんな感じ。かなり省略できる。
+    //return React.createElement(
+      //"div",
+      //null,
+      //"Hello world!"
+    //)
+  }
 }
 
 export default App;
