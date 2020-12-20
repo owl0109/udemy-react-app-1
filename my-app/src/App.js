5d695e1e228e25a58100f9e0e650928ca518838e
owl0109 <kijimakenta19960109@gmail.com>
2020-12-20 12:06:46 +0900

    Componentいじった

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,19 +1,20 @@
-import React,{Component} from 'react'
+//import React,{Component} from 'react'
 
-class App extends Component{
-  render(){
+//App => クラスコンポーネント
+//class App extends Component{
+  //render(){
     //return<div>～の箇所がJSX。可読性が高い
     //return<div>Hello World</div>
     //const greet = "Hi, Tom!"
     //const dom = <h1 className = "foo">{greet}</h1>;
     //return dom;
 
-    return(
-      <React.Fragment>
-        <label htmlFor="Bar">Bar</label>
-        <input type= "text" onChange={()=>console.log("I am Clicked!")}></input>
-      </React.Fragment>
-    );
+    //return(
+      //<React.Fragment>
+      //  <label htmlFor="Bar">Bar</label>
+      //  <input type= "text" onChange={()=>console.log("I am Clicked!")}></input>
+      //</React.Fragment>
+    //);
 
     //内部処理はこんな感じ。かなり省略できる。
     //return React.createElement(
@@ -21,7 +22,21 @@ class App extends Component{
       //null,
       //"Hello world!"
     //)
-  }
+  //}
+//}
+
+const App = () => {
+  return( 
+  <div>
+    <Cat />
+    <Cat />
+    <Cat />
+  </div>
+  );
+}
+
+const Cat = () => {
+  return <div>Yahoo!!</div>
 }
 
 export default App;
