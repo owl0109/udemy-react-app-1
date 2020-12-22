430c9eac34ebff8f1ae63e55231206b036e58ec6
owl0109 <kijimakenta19960109@gmail.com>
2020-12-22 13:48:33 +0900

    新規フォームの作成

--- my-app/src/actions/index.js
+++ my-app/src/actions/index.js
@@ -7,6 +7,8 @@
 import axios from 'axios'
 
 export const READ_EVENTS = 'READ_EVENTS'
+export const CREATE_EVENT = 'CREATE_EVENT'
+
 const ROOT_URL = 'https://udemy-utils.herokuapp.com/api/v1/'
 const QUERYSTRING = '?token=token123'
 //https://udemy-utils.herokuapp.com/api/v1/events?token=token123
@@ -16,6 +18,14 @@ export const readEvents = () => async dispatch =>{
    const response = await axios.get(ROOT_URL+"events"+QUERYSTRING)   
    //reducersに渡す
    dispatch({type: READ_EVENTS,response})
+
+}
+
+export const postEvent = values => async dispatch =>{
+   // const response = axios.get('${ROOT_URL}/events${QUERYSTRING}')
+   const response = await axios.post(ROOT_URL+"events"+QUERYSTRING,values)   
+   //reducersに渡す
+   dispatch({type: CREATE_EVENT,response})
    
 }
 
--- my-app/src/reducers/index.js
+++ my-app/src/reducers/index.js
@@ -1,8 +1,9 @@
 //アプリケーション内にあるReducerを統括する役割
 import {combineReducers} from 'redux'
+import { reducer as form} from 'redux-form'
 import events from './events'
 
-export default combineReducers({events})
+export default combineReducers({events,form})
 
 //複数状態を管理したい場合はカンマ区切りで実装する
 //export default combineReducers({count,count1,count2})
