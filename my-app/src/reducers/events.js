947ec05fb0c78324128c87e705f2514556203cdd
owl0109 <kijimakenta19960109@gmail.com>
2020-12-22 15:42:35 +0900

    削除実装

--- my-app/src/reducers/events.js
+++ my-app/src/reducers/events.js
@@ -1,10 +1,14 @@
-import {READ_EVENTS} from '../actions'
+import {READ_EVENTS,DELETE_EVENT} from '../actions'
 import _ from 'lodash'
 
 export default (events = {},action)=>{
     switch (action.type){
         case READ_EVENTS:
             return _.mapKeys(action.response.data,'id')
+        case DELETE_EVENT:
+            console.log(action.id)
+            delete events[action.id]
+            return { ...events }
         default:
             return events
     }
